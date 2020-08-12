2f28bfe812a5366c25537f4aa3eb613d7cc1b110
fabianschmauder <fabianschmauder@gmail.com>
2020-08-12 14:02:23 +0200

    Presentation start 2

--- presentation-frontend/src/pages/LandingPage.jsx
+++ presentation-frontend/src/pages/LandingPage.jsx
@@ -3,8 +3,6 @@ import ImageUploadContainer from "../components/ImageUploadContainer";
 import {analyseImage} from "../service/image-analyse-service";
 
 function LandingPage() {
-
-
     return <main>
         <ImageUploadContainer onImageDrop={(image) => {
             analyseImage(image)
